78f735fe5dd73049fd505df3028276959e963991
Bryce Denney <bryce@thedenneys.org>
2016-03-04 20:22:29 -0500

    improve the dumb script

--- dumb.pl
+++ dumb.pl
@@ -1,3 +1,13 @@
 #!/usr/bin/perl
 
-print "dumb\n";
+sub insult {
+    my ($num) = @_;
+    my $msg = "This script is so";
+    foreach (1..$num) {
+        $msg .= " dumb";
+    }
+    return $msg;
+}
+
+print insult(3);
+print insult(5);
